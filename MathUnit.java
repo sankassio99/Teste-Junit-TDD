e20ec06a04867d24a5ac7dabfd5aeea1d3e5c547
Kássio <55934898+sankassio99@users.noreply.github.com>
2020-05-07 17:49:38 -0300

    Adcionando Propriedades 8,9 e 12

--- MathUnit.java
+++ MathUnit.java
@@ -7,14 +7,48 @@ package com.mycompany.introducaojunit;
 
 /**
  *
- * @author kassi
+ * @author kassio
  */
 public class MathUnit {
     public static int mdc(int a, int b){
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior ; 
+        
         if(b > 0 && a % b == 0){
             return b;
         } 
+        
+        if(b == 0){
+            return Math.abs(a);
+        }
+        
+        if(a % b !=0){
+            return 1 ;
+        }
+        
     return -1;    
+    }
+//    public static int mdcPrimo(int p, int a){
+//
+//        if(a > 0 && p % a == 0){
+//            for(int i=2;i>p-1;i++){
+//                if(!(i%p==0)){
+//                    return p; 
+//                }
+//            }
+//        
+//        }
+//        return -1 ; 
+//    }
+
+    public static int mdc3(int a, int b, int c){
+        if(b > 0 && a % b == 0){
+            if(c > 0 && b % c == 0)
+                return c;
+        } 
+
+        return -1;
     }
 
 }
--- MathUnitTest.java
+++ MathUnitTest.java
@@ -7,7 +7,8 @@ package com.mycompany.introducaojunit;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static com.mycompany.introducaojunit.MathUnit.mdc ;
+import static com.mycompany.introducaojunit.MathUnit.mdc3 ;
 /**
  *
  * @author kassi
@@ -33,6 +34,67 @@ public class MathUnitTest {
         final int esperado = b ;
         final int obtido = MathUnit.mdc(a,b);
         assertEquals(esperado, obtido);
+    }
+    //Propriedade 02 
+    @Test
+    void testeMdcP2() {
+        final int a = 16;
+        final int b = 8;
+        final int divisor = 4 ;
+        final int esperado = 0 ;
+        final int obtido = MathUnit.mdc(a,b);
+        assertEquals(esperado, obtido % divisor);
+    }
+
+
+
+    //Propriedade 08
+    @Test
+    void testeMdcP8() {
+        final int a = 8;
+        final int b = 8;
+        final int esperado = a ;
+
+        final int obtido = mdc(a,b);
+        assertEquals(esperado, Math.abs(a));
+    };
+//
+//    //Propriedade 09
+    @Test
+    void testeMdcP9(){
+        final int a = 16;
+        final int b = 8;
+        final int c = 4 ;
+
+        final int[] arrayEsp = { c, c, c };
+        
+        
+        final int obtido1 = mdc(a, mdc(a,c));
+        final int obtido2 = mdc(mdc(a,b),c);
+        final int obtido3 = mdc3(a,b,c);
+//        System.out.println("returns: "+obtido1+ obtido2+ obtido3);
+
+        final int arrayObt [] = {obtido1, obtido2, obtido3};
+        assertArrayEquals(arrayEsp , arrayObt);
+        
+    }
+
+    //Propriedade 12
+    //Se p é um número primo mdc(p,a)=p ou mdc(p,a)=1;
+    
+    @Test
+    void testeMdc12() {
+        final int p = 7;
+        final int a = 1;
+        
+        final int esperado = p ;
+        final int esperado2 = a ;
+        
+        final int obtido = mdc(p,a) ;
+
+        System.out.println("obtido "+obtido );
+
+        assertEquals( esperado2,  obtido );
     }
     
 }
